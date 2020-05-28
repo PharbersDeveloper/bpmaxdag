37523117ef98cc97636f1cb0e673c6e4e57d4da7
yyw <384995095@qq.com>
2020-05-28 18:29:33 +0800

    job3

--- phjobs/job3_data_adding/phmain.py
+++ phjobs/job3_data_adding/phmain.py
@@ -3,10 +3,16 @@ import click
 
 
 @click.command()
-@click.option('--a')
-@click.option('--b')
-def debug_execute(a, b):
-    execute(a, b)
+@click.option('--max_path')
+@click.option('--max_path_local')
+@click.option('--project_name')
+@click.option('--model_month_right')
+@click.option('--max_month')
+@click.option('--year_missing')
+@click.option('--test_out_path')
+
+def debug_execute(max_path, max_path_local, project_name, model_month_right, max_month, year_missing, test_out_path):
+    execute(max_path, max_path_local, project_name, model_month_right, max_month, year_missing, test_out_path)
 
 
 if __name__ == '__main__':
