a9a07522998a6ee31ecd82587bc2cc43384542e5
yyw <384995095@qq.com>
2020-06-05 18:00:37 +0800

    job5

--- phjobs/job5_max/phmain.py
+++ phjobs/job5_max/phmain.py
@@ -8,10 +8,21 @@ import click
 
 
 @click.command()
-@click.option('--a')
-@click.option('--b')
-def debug_execute(a, b):
-	execute(a, b)
+@click.option('--max_path')
+@click.option('--max_path_local')
+@click.option('--project_name')
+@click.option('--if_base')
+@click.option('--time_left')
+@click.option('--time_right')
+@click.option('--left_models')
+@click.option('--time_left_models')
+@click.option('--rest_models')
+@click.option('--time_rest_models')
+@click.option('--all_models')
+@click.option('--other_models')
+@click.option('--test_out_path')
+def debug_execute(max_path, max_path_local, project_name, if_base, time_left, time_right, left_models, time_left_models, rest_models, time_rest_models, all_models, other_models, test_out_path):
+	execute(max_path, max_path_local, project_name, if_base, time_left, time_right, left_models, time_left_models, rest_models, time_rest_models, all_models, other_models, test_out_path)
 
 
 if __name__ == '__main__':
