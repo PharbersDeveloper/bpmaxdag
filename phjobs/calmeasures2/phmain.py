eceedf2092a689c80d87a411fea8958bcd02dea8
Jeorch <jeorch@foxmail.com>
2020-09-10 19:33:33 +0800

    calmeasures2 >> horcalmeasures

--- phjobs/calmeasures2/phmain.py
+++ phjobs/calmeasures2/phmain.py
@@ -5,9 +5,13 @@ This is job template for Pharbers Max Job
 from phjob import execute
 import click
 @click.command()
-@click.option('--a')
-@click.option('--b')
-def debug_execute(a, b):
-	execute(a, b)
+@click.option('--run_id')
+@click.option('--job_id')
+@click.option('--version')
+@click.option('--final_result_path')
+@click.option('--ver_measures_content_path')
+def debug_execute(**kwargs):
+    execute(**kwargs)
+
 if __name__ == '__main__':
     debug_execute()
