dba2a87c3ee05609bc9aeb139df6a3dc4542676e
zyyin <alfred>
2020-09-11 11:03:40 +0800

    create hive table 调试完成，可邮件提醒

--- phjobs/create_hive_table/phmain.py
+++ phjobs/create_hive_table/phmain.py
@@ -8,11 +8,14 @@ import click
 
 
 @click.command()
+@click.option('--input_file_format')
 @click.option('--input_path')
+@click.option('--output_file_format')
 @click.option('--output_path')
+@click.option('--save_mode')
 @click.option('--table_name')
-def debug_execute(input_path, output_path, table_name):
-	execute(input_path, output_path, table_name)
+def debug_execute(input_file_format, input_path, output_file_format, output_path, save_mode, table_name):
+	execute(input_file_format, input_path, output_file_format, output_path, save_mode, table_name)
 
 if __name__ == '__main__':
     debug_execute()
