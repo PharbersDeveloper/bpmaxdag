ff246a46c30bc3ffb2ee4a95b8da1b010abb4a81
yyw <384995095@qq.com>
2020-11-05 18:30:02 +0800

    Rawdata_pretreat

--- phjobs/Rawdata_pretreat/phmain.py
+++ phjobs/Rawdata_pretreat/phmain.py
@@ -5,9 +5,14 @@ This is job template for Pharbers Max Job
 from phjob import execute
 import click
 @click.command()
-@click.option('--a')
-@click.option('--b')
-def debug_execute(a, b):
-	execute(a, b)
+@click.option('--max_path')
+@click.option('--project_name')
+@click.option('--outdir')
+@click.option('--history_outdir')
+@click.option('--if_two_source')
+@click.option('--time_left')
+@click.option('--time_right')
+def debug_execute(max_path, project_name, outdir, history_outdir, if_two_source, time_left, time_right):
+	execute(max_path, project_name, outdir, history_outdir, if_two_source, time_left, time_right)
 if __name__ == '__main__':
     debug_execute()
