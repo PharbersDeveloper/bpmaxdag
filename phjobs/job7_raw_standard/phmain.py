a277a6ddf844f5e257649550110dd0b5ea77d4a9
yyw <384995095@qq.com>
2020-11-19 18:03:13 +0800

    job7_raw_standard

--- phjobs/job7_raw_standard/phmain.py
+++ phjobs/job7_raw_standard/phmain.py
@@ -5,9 +5,14 @@ This is job template for Pharbers Max Job
 from phjob import execute
 import click
 @click.command()
-@click.option('--a')
-@click.option('--b')
-def debug_execute(a, b):
-	execute(a, b)
+@click.option('--max_path')
+@click.option('--extract_path')
+@click.option('--project_name')
+@click.option('--if_two_source')
+@click.option('--out_dir')
+@click.option('--minimum_product_sep')
+@click.option('--minimum_product_columns')
+def debug_execute(max_path, extract_path, project_name, if_two_source, out_dir, minimum_product_sep, minimum_product_columns):
+	execute(max_path, extract_path, project_name, if_two_source, out_dir, minimum_product_sep, minimum_product_columns)
 if __name__ == '__main__':
     debug_execute()
