319ed730f81b22e255449e4bc2db7e4841156689
yyw <384995095@qq.com>
2020-12-17 15:21:49 +0800

    factor/job3_factor_optimize/

--- phjobs/factor/job3_factor_optimize/phmain.py
+++ phjobs/factor/job3_factor_optimize/phmain.py
@@ -14,13 +14,18 @@ from ph_max_auto.ph_hook.ph_hook import exec_before, exec_after
 @click.option('--owner')
 @click.option('--run_id')
 @click.option('--job_id')
-@click.option('--a')
-@click.option('--b')
+@click.option('--max_path')
+@click.option('--project_name')
+@click.option('--outdir')
+@click.option('--model_month_right')
+@click.option('--model_month_left')
+@click.option('--market_list')
+@click.option('--max_file')
 @click.option('--c')
 @click.option('--d')
 def debug_execute(**kwargs):
     try:
-        args = {'name': 'job3'}
+        args = {'name': 'job3_factor_optimize'}
 
         args.update(kwargs)
         result = exec_before(**args)
