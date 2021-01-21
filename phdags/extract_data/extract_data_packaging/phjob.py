518970b1af3f32d7dd02e3ffc86db454142ca681
Alex <891366402@qq.com>
2021-01-21 13:15:21 +0800

    权限变更导致key变更

--- phdags/extract_data/extract_data_packaging/phjob.py
+++ phdags/extract_data/extract_data_packaging/phjob.py
@@ -21,8 +21,8 @@ def execute(**kwargs):
     logger.info("当前 job_id 为 " + str(kwargs["job_id"]))
 
     awsConf = {
-        "aws_access_key_id": "AKIAWPBDTVEAI6LUCLPX",
-        "aws_secret_access_key": "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"
+        "aws_access_key_id": "AKIAWPBDTVEAPOX3QT6U",
+        "aws_secret_access_key": "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"
     }
     packaging(kwargs["from"], kwargs["out_suffix"], awsConf)
     return {}
