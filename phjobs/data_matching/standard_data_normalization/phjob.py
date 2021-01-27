5ed7c79fa9b38f726a18c8395048e71603b18f65
mzhang <mzhang@data-pharbers.com>
2021-01-27 17:22:25 +0800

    standard_data_normalization 代码重构

--- phjobs/data_matching/standard_data_normalization/phjob.py
+++ phjobs/data_matching/standard_data_normalization/phjob.py
@@ -37,10 +37,7 @@ def execute(**kwargs):
 	
 	df_standard = load_standard_prod(spark, path_master_prod)
 	df_standard.write.mode("overwrite").parquet(origin_path)
-	df_standard = df_standard.withColumn("SPEC_STANDARD_ORIGINAL", df_standard.SPEC_STANDARD) # 保留原字段内容
-	df_standard = df_standard.withColumn("SPEC", df_standard.SPEC_STANDARD)
 	df_standard = spec_standify(df_standard)
-	df_standard = df_standard.withColumn("SPEC_STANDARD", df_standard.SPEC).drop("SPEC")
 	df_standard.write.mode("overwrite").parquet(result_path)
 	
 	return {}
@@ -112,26 +109,28 @@ def load_standard_prod(spark, standard_prod_path):
 规格列规范
 """
 def spec_standify(df):
+    
 	# df = df.withColumn("SPEC_ORIGINAL", df.SPEC)
 # 	spec_valid_regex = r'([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)'
 # 	spec_gross_regex = r'([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)[ ,/:∶+\s][\u4e00-\u9fa5]*([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)'
 	spec_valid_regex = r'(\d{0,}[.]{0,1}\d+[MU]{0,1}G|\d{0,}[.]{0,1}\d+[ITM]U[G]{0,1}|\d{0,}[.]{0,1}\d+(AXAIU)|\d{0,}[.]{0,1}\d+(AXAU)|\d{0,}[.]{0,1}\d+(TIU)|\d{0,}[.]{0,1}\d+[Y])'
 	spec_gross_regex =  r'(\d{0,}[.]{0,1}\d+[M]{0,1}L|\d{0,}[.]{0,1}\d+[ITM]U[G]{0,1}|\d{0,}[.]{0,1}\d+[CM]M)'
 # 	spec_third_regex = r'([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)[ /:∶+\s]([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)[ /:∶+\s]([0-9]\d*\.?\d*\s*[A-Za-z]*/?\s*[A-Za-z]+)'
-	df = df.withColumn("SPEC", upper(df.SPEC))\
-			.withColumn("SPEC", regexp_replace("SPEC", r"(万)", "T"))\
-			.withColumn("SPEC", regexp_replace("SPEC", r"(μ)", "U"))\
-			.withColumn("SPEC", regexp_replace("SPEC", r"(ΜG)", "MG"))\
+	df = df.withColumn("SPEC_STANDARD_ORIGINAL", df.SPEC_STANDARD)\
+			.withColumn("SPEC_STANDARD", upper(df.SPEC_STANDARD))\
+			.withColumn("SPEC_STANDARD", regexp_replace("SPEC_STANDARD", r"(万)", "T"))\
+			.withColumn("SPEC_STANDARD", regexp_replace("SPEC_STANDARD", r"(μ)", "U"))\
+			.withColumn("SPEC_STANDARD", regexp_replace("SPEC_STANDARD", r"(ΜG)", "MG"))\
 			.replace(" ", "")\
-			.withColumn("SPEC_percent", regexp_extract('SPEC', r'(\d{1,3}[.]{0,1}\d+%)', 1))\
-			.withColumn("SPEC_valid", regexp_extract('SPEC', spec_valid_regex, 1))\
-			.withColumn("SPEC_gross", regexp_extract('SPEC', spec_gross_regex, 1))\
+			.withColumn("SPEC_percent", regexp_extract('SPEC_STANDARD', r'(\d{1,3}[.]{0,1}\d+%)', 1))\
+			.withColumn("SPEC_valid", regexp_extract('SPEC_STANDARD', spec_valid_regex, 1))\
+			.withColumn("SPEC_gross", regexp_extract('SPEC_STANDARD', spec_gross_regex, 1))\
 			.na.fill("")
 
 # 	df = df.withColumn("SPEC_percent", percent_pandas_udf(df.SPEC_percent, df.SPEC_valid, df.SPEC_gross))
 	df = df.withColumn("SPEC_valid", transfer_unit_pandas_udf(df.SPEC_valid))
 	df = df.withColumn("SPEC_gross", transfer_unit_pandas_udf(df.SPEC_gross))
-	df = df.withColumn("SPEC", concat_ws('/',df.SPEC_percent ,df.SPEC_valid , df.SPEC_gross))\
+	df = df.withColumn("SPEC_STANDARD", concat_ws('/',df.SPEC_percent ,df.SPEC_valid , df.SPEC_gross))\
 			.drop("SPEC_percent", "SPEC_valid", "SPEC_gross")
 	return df
 
