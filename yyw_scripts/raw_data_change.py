56658e56b5105bd5162aa80c6e6003aad40c3be3
yyw <384995095@qq.com>
2021-01-27 10:39:05 +0800

    raw_data_change 完善

--- yyw_scripts/raw_data_change.py
+++ yyw_scripts/raw_data_change.py
@@ -52,13 +52,16 @@ change_file = change_file.fillna({"Form":"NA","Specifications":"NA","Pack_Number
 
 # 产品层面
 change_file_1 = change_file[change_file['错误类型'] == '产品层面']
-change_file_1[['Pack_Number']] = change_file_1[['Pack_Number']].astype(int)
-change_file_1[['Brand','Pack_Number', 'Date', 'Hospital_ID']] = change_file_1[['Brand','Pack_Number', 'Date', 'Hospital_ID']].astype(str)
-change_file_1['all_info'] = (change_file_1['Molecule'] + change_file_1['Brand'] + change_file_1['Form'] + 
-                                change_file_1['Specifications'] + change_file_1['Pack_Number'] + 
-                                change_file_1['Manufacturer'] + change_file_1['Date'] + change_file_1['Hospital_ID']).str.strip()
-change_file_1_spark = spark.createDataFrame(change_file_1)
-change_file_1_spark = change_file_1_spark.select('all_info', 'Sales_old', 'Sales_new', 'Units_old', 'Units_new')
+if len(change_file_1) >0:
+    change_file_1[['Pack_Number']] = change_file_1[['Pack_Number']].astype(int)
+    change_file_1[['Brand','Pack_Number', 'Date', 'Hospital_ID']] = change_file_1[['Brand','Pack_Number', 'Date', 'Hospital_ID']].astype(str)
+    change_file_1['all_info'] = (change_file_1['Molecule'] + change_file_1['Brand'] + change_file_1['Form'] + 
+                                    change_file_1['Specifications'] + change_file_1['Pack_Number'] + 
+                                    change_file_1['Manufacturer'] + change_file_1['Date'] + change_file_1['Hospital_ID']).str.strip()
+    change_file_1_spark = spark.createDataFrame(change_file_1)
+    change_file_1_spark = change_file_1_spark.select('all_info', 'Sales_old', 'Sales_new', 'Units_old', 'Units_new')
+    print('产品层面替换信息')
+    change_file_1_spark.show()
 
 # 医院层面
 change_file_2 = change_file[change_file['错误类型'] == '医院层面']
@@ -67,6 +70,7 @@ if len(change_file_2) >0:
     change_file_2 = change_file_2[['Date', 'Hospital_ID', '医院层面替换月份']]
     change_file_2_spark = spark.createDataFrame(change_file_2)
     change_file_2_spark = change_file_2_spark.withColumnRenamed('Hospital_ID', 'ID')
+    print('医院层面替换信息')
     change_file_2_spark.show()
 
 # 2. 对raw_data修改并备份
@@ -74,35 +78,43 @@ def change_raw(raw_data_old_path, raw_data_new_path):
     raw_data_old = spark.read.parquet(raw_data_old_path)
     
     # a. 产品层面
-    raw_data_old = raw_data_old.withColumn('Brand_new', func.when(raw_data_old.Brand.isNull(), func.lit('nan')).otherwise(raw_data_old.Brand))
-    raw_data_old = raw_data_old.withColumn('all_info', func.concat(func.col('Molecule'), func.col('Brand_new'), func.col('Form'), func.col('Specifications'),
-                             func.col('Pack_Number'), func.col('Manufacturer'), func.col('Date'), func.col('ID')))
-    raw_data_new = raw_data_old.join(change_file_1_spark, on='all_info', how='left')
-    
-    # 检查替换的信息是否正确
-    print("产品层面替换的条目：", change_file_1_spark.count())
-    print("匹配到的替换的条目：", raw_data_new.where(~raw_data_new.Sales_old.isNull()).count())
-    
-    raw_data_new.where(~raw_data_new.Sales_old.isNull()) \
-            .select('all_info', 'Sales', 'Sales_old', 'Sales_new', 'Units', 'Units_old', 'Units_new').show()
-    
-    # 替换数据
-    raw_data_new = raw_data_new.withColumn('Sales', func.when(~raw_data_new.Sales_old.isNull(), raw_data_new.Sales_new) \
-                                                        .otherwise(raw_data_new.Sales)) \
-                            .withColumn('Units', func.when(~raw_data_new.Units_old.isNull(), raw_data_new.Units_new) \
-                                                        .otherwise(raw_data_new.Units))
-    
-    raw_data_new.where(~raw_data_new.Sales_old.isNull()) \
-            .select('all_info', 'Sales', 'Sales_old', 'Sales_new', 'Units', 'Units_old', 'Units_new').show()
-            
-    raw_data_new = raw_data_new.drop('all_info', 'Sales_old', 'Sales_new', 'Units_old', 'Units_new', 'Brand_new')
-    
-    print('修改前后raw_data行数是否一致：', (raw_data_new.count() == raw_data_old.count()))
+    if len(change_file_1) >0:
+        raw_data_old = raw_data_old.withColumn('Brand_new', func.when(raw_data_old.Brand.isNull(), func.lit('nan')).otherwise(raw_data_old.Brand))
+        raw_data_old = raw_data_old.withColumn('all_info', func.concat(func.col('Molecule'), func.col('Brand_new'), func.col('Form'), 
+                                                                       func.col('Specifications'), func.col('Pack_Number'), func.col('Manufacturer'),
+                                                                       func.col('Date'), func.col('ID')))
+        raw_data_new = raw_data_old.join(change_file_1_spark, on='all_info', how='left')
+
+        # 检查替换的信息是否正确
+        print("产品层面替换的条目：", change_file_1_spark.count())
+        print("匹配到的替换的条目：", raw_data_new.where(~raw_data_new.Sales_old.isNull()).count())
+        
+        print("替换前")
+        raw_data_new.where(~raw_data_new.Sales_old.isNull()) \
+                .select('all_info', 'Sales', 'Sales_old', 'Sales_new', 'Units', 'Units_old', 'Units_new').show()
+
+        # 替换数据
+        raw_data_new = raw_data_new.withColumn('Sales', func.when(~raw_data_new.Sales_old.isNull(), raw_data_new.Sales_new) \
+                                                            .otherwise(raw_data_new.Sales)) \
+                                .withColumn('Units', func.when(~raw_data_new.Units_old.isNull(), raw_data_new.Units_new) \
+                                                            .otherwise(raw_data_new.Units))
+        print("替换后")
+        raw_data_new.where(~raw_data_new.Sales_old.isNull()) \
+                .select('all_info', 'Sales', 'Sales_old', 'Sales_new', 'Units', 'Units_old', 'Units_new').show()
+
+        raw_data_new = raw_data_new.drop('all_info', 'Sales_old', 'Sales_new', 'Units_old', 'Units_new', 'Brand_new')
+
+        print('修改前后raw_data行数是否一致：', (raw_data_new.count() == raw_data_old.count()))
     
     # b. 医院层面
     if len(change_file_2) >0:
-        raw_data_new_drop = raw_data_new.join(change_file_2_spark, on= ['Date', 'ID'], how='left_anti')
-        raw_data_new_replace = raw_data_new.join(change_file_2_spark.withColumnRenamed('Date', 'Date_raw').withColumnRenamed('医院层面替换月份', 'Date'), 
+        print("医院层面替换：")
+        if len(change_file_1) >0:
+            raw_df = raw_data_new.
+        else:
+            raw_df = raw_data_old
+        raw_data_new_drop = raw_df.join(change_file_2_spark, on= ['Date', 'ID'], how='left_anti')
+        raw_data_new_replace = raw_df.join(change_file_2_spark.withColumnRenamed('Date', 'Date_raw').withColumnRenamed('医院层面替换月份', 'Date'), 
                                                 on=['Date', 'ID'] ,how='inner')
         raw_data_new_replace = raw_data_new_replace.withColumn('Date', raw_data_new_replace['Date_raw']).drop('Date_raw')
         
