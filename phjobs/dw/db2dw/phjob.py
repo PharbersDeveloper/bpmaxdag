f7bc865cbf316a376d3bed2a90f20aaad2b45be6
AlfredYang1986 <yangyuanpig@gmail.com>
2021-03-08 13:51:05 +0800

    更换dw数据版本

--- phjobs/dw/db2dw/phjob.py
+++ phjobs/dw/db2dw/phjob.py
@@ -6,6 +6,7 @@ This is job template for Pharbers Max Job
 
 import random
 import string
+import json
 from phcli.ph_logs.ph_logs import phs3logger, LOG_DEBUG_LEVEL
 from pyspark.sql.functions import lit, udf
 from pyspark.sql.types import StringType
@@ -13,6 +14,14 @@ from pyspark.sql import SparkSession
 
 
 def execute(**kwargs):
+    def camel_to_underline(format_str):
+        underline_format = ""
+        if isinstance(format_str, str):
+            for _s_ in format_str:
+                underline_format += _s_ if _s_.islower() else "_" + _s_.lower()
+        return format_str + " as " + underline_format.upper()
+        
+        
     """
         please input your code below
         get spark session: spark = kwargs["spark"]()
@@ -21,16 +30,24 @@ def execute(**kwargs):
     logger.info("当前 owner 为 " + str(kwargs["owner"]))
     logger.info("当前 run_id 为 " + str(kwargs["run_id"]))
     logger.info("当前 job_id 为 " + str(kwargs["job_id"]))
-    # spark = kwargs["spark"]()
+    spark = kwargs["spark"]()
 
-    spark = SparkSession.builder.getOrCreate()
+    # spark = SparkSession.builder.getOrCreate()
 
-    postgres_uri = kwargs["db_uri"]
-    postgres_properties = {'user': kwargs["db_user"], 'password': kwargs["db_password"]}
-
-    tables = kwargs["tables"]
-    for table in tables:
-        reading = spark.read.jdbc(url=postgres_uri, table=table, properties=postgres_properties)
-        reading.show()
+    _postgres_uri = kwargs["db_uri"]
+    _postgres_properties = {'user': kwargs["db_user"], 'password': kwargs["db_password"]}
+    _version = kwargs["version"]
+    
+    tables = json.loads(kwargs["db_tables"].replace("'", '"'))
+    for item in tables:
+        reading = spark.read.jdbc(url=_postgres_uri, table=item["table_name"], properties=_postgres_properties)
+        columns =  reading.drop("category").drop("product").columns
+        reading \
+            .selectExpr(*list(map(camel_to_underline, columns))) \
+            .withColumn("VERSION", lit(_version)) \
+            .repartition(1) \
+            .write.mode("overwrite") \
+            .parquet(item["dw_path"] + _version)
 
     return {}
+
--- phjobs/dw/db2dw/phmain.py
+++ phjobs/dw/db2dw/phmain.py
@@ -14,6 +14,7 @@ from phcli.ph_max_auto.ph_hook.ph_hook import exec_before, exec_after
 @click.option('--owner')
 @click.option('--run_id')
 @click.option('--job_id')
+@click.option('--version')
 @click.option('--db_uri')
 @click.option('--db_user')
 @click.option('--db_password')
