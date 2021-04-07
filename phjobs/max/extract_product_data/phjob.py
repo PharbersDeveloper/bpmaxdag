a535b5d0004cec64cf3b4aaef4798b0be6b6febe
AlfredYang1986 <yangyuanpig@gmail.com>
2021-04-07 14:56:36 +0800

    重置group

---
+++ phjobs/max/extract_product_data/phjob.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""alfredyang@pharbers.com.
+
+This is job template for Pharbers Max Job
+"""
+
+from phcli.ph_logs.ph_logs import phs3logger, LOG_DEBUG_LEVEL
+
+
+def execute(**kwargs):
+    """
+        please input your code below
+        get spark session: spark = kwargs["spark"]()
+    """
+    logger = phs3logger(kwargs["job_id"], LOG_DEBUG_LEVEL)
+    logger.info("当前 owner 为 " + str(kwargs["owner"]))
+    logger.info("当前 run_id 为 " + str(kwargs["run_id"]))
+    logger.info("当前 job_id 为 " + str(kwargs["job_id"]))
+    spark = kwargs["spark"]()
+    logger.info(kwargs["a"])
+    logger.info(kwargs["b"])
+    logger.info(kwargs["c"])
+    logger.info(kwargs["d"])
+    return {}
