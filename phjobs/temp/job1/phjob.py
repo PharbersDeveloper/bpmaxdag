2db7b8553a843e7b9842d1a5e9db5bf29452213e
mzhang <mzhang@data-pharbers.com>
2021-04-22 15:16:24 +0800

    流程中增加功能，收集被cutting掉的数据，本地测试成功

--- phjobs/temp/job1/phjob.py
+++ phjobs/temp/job1/phjob.py
@@ -32,36 +32,39 @@ def execute(**kwargs):
 
 
 #######################-------test--------------##################
-    url_pos = r"s3a://ph-max-auto/2020-08-11/data_matching/refactor/results/2021-04-21_10-18-15/Positive_Predictions/"
-    df_pos = spark.read.csv(url_pos,header=True)
+#     url_pos = r"s3a://ph-max-auto/2020-08-11/data_matching/refactor/results/2021-04-21_10-18-15/Positive_Predictions/"
+#     df_pos = spark.read.csv(url_pos,header=True)
     
-    url_raw = r"s3a://ph-max-auto/2020-08-11/data_matching/refactor/runs/manual__2021-04-21T09_34_29.804083+00_00/cleaning_data_normalization/cleaning_origin/"
-    df_raw = spark.read.parquet(url_raw)
-    print(df_pos.printSchema())
-    print(df_raw.printSchema())
-    df_raw = df_raw.withColumnRenamed("MOLE_NAME","MOLE_ORIGINAL")\
-                    .withColumnRenamed("PRODUCT_NAME","PRODUCT_ORIGINAL")\
-                    .withColumnRenamed("SPEC","SPEC_ORIGINAL")\
-                    .withColumnRenamed("DOSAGE","DOSAGE_ORIGINAL")\
-                    .withColumnRenamed("MANUFACTURER_NAME","MANUFACTURER_NAME_ORIGINAL")\
-                    .withColumnRenamed("PACK_ID_CHECK","PACK_ID_CHECK_ORIGINAL_")\
-                    .withColumnRenamed("ID","ID_ORIGINAL")\
-                    .withColumnRenamed("pack_qty","PACK_QTY_ORIGINAL_")
-    cols = ["MOLE_ORIGINAL",\
-           "PRODUCT_ORIGINAL",\
-           "SPEC_ORIGINAL",\
-           "DOSAGE_ORIGINAL",\
-           "MANUFACTURER_NAME_ORIGINAL",\
-           "PACK_ID_CHECK_ORIGINAL_",\
-           "ID_ORIGINAL",\
-           "PACK_QTY_ORIGINAL_"]
-    df_raw = df_raw.select(cols)
-    df_raw.show(100)
-    print(df_raw.printSchema())
-    df = df_pos.join(df_raw,df_pos.ID == df_raw.ID_ORIGINAL,'left')
-    df.repartition(1).write.mode("overwrite").csv("s3a://ph-max-auto/2020-08-11/data_matching/temp/mzhang/0421_pos_test",header=True)
+#     url_raw = r"s3a://ph-max-auto/2020-08-11/data_matching/refactor/runs/manual__2021-04-21T09_34_29.804083+00_00/cleaning_data_normalization/cleaning_origin/"
+#     df_raw = spark.read.parquet(url_raw)
+#     print(df_pos.printSchema())
+#     print(df_raw.printSchema())
+#     df_raw = df_raw.withColumnRenamed("MOLE_NAME","MOLE_ORIGINAL")\
+#                     .withColumnRenamed("PRODUCT_NAME","PRODUCT_ORIGINAL")\
+#                     .withColumnRenamed("SPEC","SPEC_ORIGINAL")\
+#                     .withColumnRenamed("DOSAGE","DOSAGE_ORIGINAL")\
+#                     .withColumnRenamed("MANUFACTURER_NAME","MANUFACTURER_NAME_ORIGINAL")\
+#                     .withColumnRenamed("PACK_ID_CHECK","PACK_ID_CHECK_ORIGINAL_")\
+#                     .withColumnRenamed("ID","ID_ORIGINAL")\
+#                     .withColumnRenamed("pack_qty","PACK_QTY_ORIGINAL_")
+#     cols = ["MOLE_ORIGINAL",\
+#            "PRODUCT_ORIGINAL",\
+#            "SPEC_ORIGINAL",\
+#            "DOSAGE_ORIGINAL",\
+#            "MANUFACTURER_NAME_ORIGINAL",\
+#            "PACK_ID_CHECK_ORIGINAL_",\
+#            "ID_ORIGINAL",\
+#            "PACK_QTY_ORIGINAL_"]
+#     df_raw = df_raw.select(cols)
+#     df_raw.show(100)
+#     print(df_raw.printSchema())
+#     df = df_pos.join(df_raw,df_pos.ID == df_raw.ID_ORIGINAL,'left')
+#     df.repartition(1).write.mode("overwrite").csv("s3a://ph-max-auto/2020-08-11/data_matching/temp/mzhang/0421_pos_test",header=True)
     
-    
+    url = r"s3a://ph-max-auto/2020-08-11/data_matching/refactor/runs/manual__2021-04-21T09_34_29.804083+00_00/cross_join_cutting/cross_result/"
+    df = spark.read.parquet(url)
+    df = df.select("ID").distinct()
+    print(df.count())
     
 #####################-------test---------------################
 
