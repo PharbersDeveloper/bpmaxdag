ded72e7d0c462610bda6f85ad30cf9027ea3feb9
yyw <384995095@qq.com>
2021-06-30 10:15:43 +0000

    job2:增加参数g_database_input，g_database_tmp，输出放函数里

--- phjobs/max/job2_product_mapping/phmain.py
+++ phjobs/max/job2_product_mapping/phmain.py
@@ -25,6 +25,8 @@ from phcli.ph_max_auto.ph_hook.ph_hook import exec_before, exec_after
 @click.option('--run_id')
 @click.option('--owner')
 @click.option('--g_input_version')
+@click.option('--g_database_temp')
+@click.option('--g_database_input')
 @click.option('--g_out_table')
 @click.option('--g_need_clean_table')
 def debug_execute(**kwargs):
