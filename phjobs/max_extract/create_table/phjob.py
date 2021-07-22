0ab05b28872cd3b9744574c0db55279a700e0fc2
yyw <384995095@qq.com>
2021-07-22 11:09:01 +0000

    生成提数大表增加date分区，update_table 对提供的路径对大表分区进行更新

--- phjobs/max_extract/create_table/phjob.py
+++ phjobs/max_extract/create_table/phjob.py
@@ -32,40 +32,47 @@ def execute(**kwargs):
     from pyspark.sql.functions import pandas_udf, PandasUDFType, udf, col
     import json
     import boto3  
-    import time    # %%
+    import time    
+    # %%
     l_paths = g_path.replace(' ','').split(',')
-    
-    
+    # %%
     # =========== 函数定义：输出结果 =============
+    
     def createPartition(path):
         df = spark.read.parquet(path)
         df = df.withColumn('filetype', func.lit(filetype))
         recordCount = df.count()
         l_dict_scheme = getScheme(df)
-        
-        # 创建分区
-        logger.debug('创建分区')
+            
         Location = path
         run_id = path.split('/version=')[1].split('/')[0]
         provider = path.split('/provider=')[1].split('/')[0]
         #owner = path.split('/owner=')[1].split('/')[0]
         
-        partition_input_list = [{
-         "Values": [run_id, provider, filetype], 
-        "StorageDescriptor": 
-            {
-            "SerdeInfo": {"SerializationLibrary": "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"}, 
-            "Location": Location, 
-            "Columns": l_dict_scheme,
-            "Parameters": {"compressionType": "none", 
-                        "classification": "parquet", 
-                        "typeOfData": "file", 
-                        "recordCount": str(recordCount)}
-            } 
-        }]    
-        client = boto3.client('glue')
-        glue_info = client.batch_create_partition(DatabaseName=g_database_result, TableName=g_out_table, PartitionInputList=partition_input_list)
-        logger.debug(glue_info)
+        pdf=df.select('DATE', 'DOI').distinct().toPandas()
+            
+        for indexs in pdf.index:
+            data = pdf.loc[indexs]
+            i_data = data['DATE']
+            i_doi = data['DOI']      
+            
+            # 创建分区
+            logger.debug('创建分区')    
+            partition_input_list = [{
+             "Values": [run_id, provider, filetype, str(i_data)], 
+            "StorageDescriptor": 
+                {
+                "SerdeInfo": {"SerializationLibrary": "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"}, 
+                "Location": Location, 
+                "Columns": l_dict_scheme,
+                "Parameters": {"compressionType": "none", 
+                            "classification": "parquet", 
+                            "typeOfData": "file"}
+                } 
+            }]    
+            client = boto3.client('glue')
+            glue_info = client.batch_create_partition(DatabaseName=g_database_result, TableName=g_out_table, PartitionInputList=partition_input_list)
+            logger.debug(glue_info)
         
     def getScheme(df):
         file_scheme = df.dtypes
@@ -77,7 +84,8 @@ def execute(**kwargs):
             l_dict_scheme.append(dict_scheme)
         return l_dict_scheme
             
+
+    # %%
     for path in l_paths:
         logger.debug(path)
         createPartition(path)
-
