6ae1014ae109c599d78b8311fb487ad433d5eed0
yyw <384995095@qq.com>
2021-09-10 07:01:47 +0000

    max_check, 增加省份城市贡献度结果

--- phjobs/max/check/phmain.py
+++ phjobs/max/check/phmain.py
@@ -44,6 +44,34 @@ def debug_execute(**kwargs):
         logger.error(traceback.format_exc())
         print(traceback.format_exc())
         raise e
+        
+@click.command()
+@click.option('--owner')
+@click.option('--dag_name')
+@click.option('--run_id')
+@click.option('--job_full_name')
+@click.option('--job_id')
+@click.option('--job_args_name')
+def online_debug_execute(**kwargs):
+    try:
+        args = {"name": "check"}
+        outputs = ["c", "d"]
+        
+        args.update(kwargs)
+        result = exec_before(**args)
+
+        args.update(result if isinstance(result, dict) else {})
+        result = execute(**args)
+
+        args.update(result if isinstance(result, dict) else {})
+        result = exec_after(outputs=outputs, **args)
+
+        return result
+    except Exception as e:
+        logger = phs3logger(kwargs["job_id"])
+        logger.error(traceback.format_exc())
+        print(traceback.format_exc())
+        raise e
 
 
 if __name__ == '__main__':
