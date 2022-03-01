2aa945dffdf4e77bbcc62180e442b6f12af8fe52
yyw <384995095@qq.com>
2022-03-01 07:02:30 +0000

    project_sample,project_sample_hz:union优化，first优化-ignorenulls=True

--- phjobs/cmax/project_sample/phjob.py
+++ phjobs/cmax/project_sample/phjob.py
@@ -83,6 +83,20 @@ def execute(**kwargs):
     def reName(df, dict_rename={}):
         df = reduce(lambda df, i_dict:df.withColumnRenamed(i_dict[0], i_dict[1]), zip(dict_rename.keys(), dict_rename.values()), df)
         return df
+    
+    def unionDf(df1, df2, utype='same'):
+        if utype=='same':
+            all_cols =  list(set(df1.columns).intersection(set(df2.columns)) - set(['version']))
+        elif utype=='all':
+            all_cols =  list(set(set(df1.columns + df2.columns) - set(['version'])))     
+            for i in all_cols:
+                if i not in df1.columns:
+                    df1 = df1.withColumn(i, func.lit(None))
+                if i not in df2.columns:
+                    df2 = df2.withColumn(i, func.lit(None))            
+        df_all = df1.select(all_cols).union(df2.select(all_cols)) 
+        return df_all    
+    
     ##---- Universe info ----
     ## projection data
     def getProjData(df_imp_total):
@@ -100,9 +114,9 @@ def execute(**kwargs):
     ##---- Sample province projection ----
     def getDistrictMapping(df_hospital_universe):
         ## district mapping
-        df_district_mapping = df_hospital_universe.withColumn('est', func.first('est') \
+        df_district_mapping = df_hospital_universe.withColumn('est', func.first('est', ignorenulls=True) \
                                                         .over(Window.partitionBy('province', 'city', 'district', 'pchc', 'flag_sample') \
-                                                        .orderBy( *[col(i).asc_nulls_last() for i in ['est'] ] ))) \
+                                                        .orderBy())) \
                                                 .groupby('province', 'city', 'district') \
                                                         .agg(func.sum('flag_sample').alias('sample'), func.expr('percentile_approx(est, 0.5)').alias('est')) \
                                                 .orderBy(col('province'), col('city'), col('est').desc())    
@@ -127,7 +141,7 @@ def execute(**kwargs):
                                             .where(col('est_flag') > 0) \
                                             .withColumn('est_flag_min', func.min('est_flag').over(Window.partitionBy('province', 'city', 'district').orderBy()))  \
                                             .where(col('est_flag') == col('est_flag_min')) \
-                                            .withColumn('sample_dist_first', func.first('sample_dist').over(Window.partitionBy('province', 'city', 'district').orderBy('sample_dist')))  \
+                                            .withColumn('sample_dist_first', func.first('sample_dist', ignorenulls=True).over(Window.partitionBy('province', 'city', 'district').orderBy()))  \
                                             .where(col('sample_dist') == col('sample_dist_first'))
         df_proj_sample2 = df_proj_sample1.select('province', 'city', 'district', 'sample_city', 'sample_dist') \
                                         .join(df_hospital_universe, on=['province', 'city', 'district'], how='left') \
@@ -143,8 +157,8 @@ def execute(**kwargs):
                                         .withColumn('units_est_ratio', col('units_mean')/col('est_mean') )
         df_proj_sample4 =  df_proj_sample3.withColumn('count', func.count('sample_dist').over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
                                     .withColumn('flag_sample_sum', func.sum('flag_sample').over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
-                                    .withColumn('sales_est_ratio_m', func.first('sales_est_ratio').over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
-                                    .withColumn('units_est_ratio_m', func.first('units_est_ratio').over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
+                                    .withColumn('sales_est_ratio_m', func.first('sales_est_ratio', ignorenulls=True).over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
+                                    .withColumn('units_est_ratio_m', func.first('units_est_ratio', ignorenulls=True).over(Window.partitionBy('sample_dist', 'market', 'packid', 'date').orderBy())) \
                                     .withColumn('sample_ratio', col('flag_sample_sum')/col('count') ) \
                                     .withColumn('sales', func.when( (col('flag_sample')==1) & (col('sample_ratio') >= 0.8), col('sales'))
                                                             .otherwise( col('est')*col('sales_est_ratio_m') ) )  \
@@ -152,18 +166,17 @@ def execute(**kwargs):
                                                             .otherwise( col('est')*col('units_est_ratio_m') ) )      
         return df_proj_sample4
     def getProjSampleFinal(df_proj_sample, df_out_data, df_imp_others='NA', df_out_sample = 'NA'):
-        all_cols =  list(set(df_out_data.columns).intersection(set(df_proj_sample.columns)) - set(['version']))
-        df_proj_sample_all = df_proj_sample.select(all_cols).union(df_out_data.select(all_cols))
+        df_proj_sample_all = unionDf(df_proj_sample, df_out_data, utype='same')
         if df_out_sample != 'NA':
             df_proj_sample_all = df_proj_sample_all.join(df_out_sample, on='pchc', how='left_anti')
         df_proj_sample_all_final = df_proj_sample_all.groupby('date', 'province', 'city', 'district', 'market', 'packid', 'flag_sample') \
                                                 .agg(func.sum('sales').alias('sales'), func.sum('units').alias('units')) \
                                                 .where(col('sales') > 0.0).where(col('units') > 0.0)
         if df_out_sample != 'NA':
-            all_cols =  list(set(df_proj_sample_all_final.columns).intersection(set(df_imp_others.columns)) - set(['version']))
-            df_proj_sample_all_final = df_proj_sample_all_final.select(all_cols).union(df_imp_others.select(all_cols))
+            df_proj_sample_all_final = unionDf(df_proj_sample_all_final, df_imp_others, utype='same')
         return df_proj_sample_all_final
     
+    # %%
     # =========== 数据执行 =============
     def projSamplePip(df_imp_total, df_hospital_universe, df_imp_others, df_out_sample):
         ##---- Sample province projection ----
@@ -174,9 +187,8 @@ def execute(**kwargs):
         df_district_sample = getDistrictSample(df_district_mapping)
         df_proj_sample = getProjSample(df_district_mapping, df_district_sample, df_hospital_universe, df_proj_data, df_sample_pack)
         df_proj_sample_final = getProjSampleFinal(df_proj_sample, df_out_data, df_imp_others, df_out_sample)
-
         return df_proj_sample_final
-    
+
     df_proj_sample_final = projSamplePip(df_imp_total, df_hospital_universe, df_imp_others, df_out_sample)
     
     # %%
